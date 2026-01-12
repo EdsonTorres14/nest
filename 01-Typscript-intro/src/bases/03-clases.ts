04ee58b7511b9acb233bd1ab4d95e08b95c1a742
EdsonTorres14 <edsontorresrios@gmail.com>
2026-01-12 12:10:12 -0600

    TypeScript - Clases y forma abreviada

---
+++ 01-Typscript-intro/src/bases/03-clases.ts
@@ -0,0 +1,20 @@
+// export class Pokemon {
+//     public id: number;
+//     public name: string;
+
+//     constructor(id: number, name: string) {
+//         this.id = id;
+//         this.name = name;
+//         console.log('constructor llamado')
+//     }
+// }
+
+// Forma corta
+
+export class Pokemon {
+    constructor(
+        public readonly id: number,
+        public name: string
+    ) { }
+}
+export const charmander = new Pokemon(4, 'Charmander')
--- 01-Typscript-intro/src/main.ts
+++ 01-Typscript-intro/src/main.ts
@@ -1,10 +1,11 @@
 // import { name } from './bases/01-types';
-import { bulbasaur } from './bases/02-objects';
+// import { bulbasaur } from './bases/02-objects';
 
+import { charmander } from './bases/03-clases';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>Hola ${ bulbasaur.name }!!! </h1>
+  <h1>Hola ${charmander.name}!!! </h1>
 `;
