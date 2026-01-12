5cd41a6a0d94b7fc9a0535e38145ee1271bb4f16
EdsonTorres14 <edsontorresrios@gmail.com>
2026-01-12 17:31:06 -0600

    TypeScript  - Decoradores

--- 01-Typscript-intro/src/main.ts
+++ 01-Typscript-intro/src/main.ts
@@ -1,8 +1,8 @@
 // import { name } from './bases/01-types';
 // import { bulbasaur } from './bases/02-objects';
 // import { charmander } from './bases/03-clases';
-
-import { charmander } from './bases/04-injection';
+// import { charmander } from './bases/04-injection';
+import { charmander } from './bases/05-decorators';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
