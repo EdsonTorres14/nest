b6260b7e26187724b6469b7042e3aca65d4203e3
EdsonTorres14 <edsontorresrios@gmail.com>
2026-01-12 02:24:47 -0600

    TypeScript - Tipos de datos - continuación

--- 01-Typscript-intro/src/bases/01-types.ts
+++ 01-Typscript-intro/src/bases/01-types.ts
@@ -1,2 +1,17 @@
-export const name = 'Edson';
+export let name: string = 'Edson';
+export const age: number = 35;
+export const isValid: boolean = true;
 
+
+name = 'David';
+
+export const templateString = ` Esto es un string 
+nultilinea
+que puede tener
+" dobles
+' simple
+inyectar valores ${ name }
+expresiones ${ 1 + 1 }
+numeros ${ age }
+booleanos ${ isValid }
+`
