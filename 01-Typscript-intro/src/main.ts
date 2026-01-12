17ed13585e252ac6c9b9b0a77984135770b30a02
EdsonTorres14 <edsontorresrios@gmail.com>
2026-01-12 13:19:30 -0600

    TypeScript - Inyección de dependencias

--- 01-Typscript-intro/src/main.ts
+++ 01-Typscript-intro/src/main.ts
@@ -1,7 +1,8 @@
 // import { name } from './bases/01-types';
 // import { bulbasaur } from './bases/02-objects';
+// import { charmander } from './bases/03-clases';
 
-import { charmander } from './bases/03-clases';
+import { charmander } from './bases/04-injection';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
